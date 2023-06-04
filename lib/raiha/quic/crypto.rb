1fcb33a89b458a6a5d00e749f6df2d8f803e24e2
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-06-05 00:34:45 +0900

    Raiha::Quic::Crypto (skeleton)

---
+++ lib/raiha/quic/crypto.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Raiha::Quic
+  module Crypto
+    class HeaderProtection
+
+      # @param cipher_name [String] cipher name
+      # @param key [String] key
+      def initialize(cipher_name:, key:)
+      end
+
+      def apply
+      end
+
+      def remove
+      end
+    end
+  end
+end
