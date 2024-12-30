f765101b268d17819b20a49f3372cfead8aef755
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-12-30 03:41:06 +0000

    Create tls extension abstract class

---
+++ lib/raiha/tls/protocol/handshake/extension/abstract_extension.rb
@@ -0,0 +1,21 @@
+require_relative "../extension"
+
+class Raiha::TLS::Protocol::Handshake
+  class Extension
+    # Abstract class of each extensions.
+    # Defines `extension_data` accessor method only.
+    # Each properties of each extensions (e.g. `server_name`, `supported_groups`, etc.) are defined in each extension classes.
+    class AbstractExtension
+      EXTENSION_TYPE_NUMBER = nil # If not override this const, #serialize method will raise error.
+
+      def extension_data=(data)
+        @exention_data = data
+      end
+
+      def serialize
+        packed_extension_data = extension_data.pack("C*")
+        [EXTENSION_TYPE_NUMBER].pack("n") + [packed_extension_data.bytesize].pack("n") + packed_extension_data
+      end
+    end
+  end
+end
