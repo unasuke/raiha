bc81d1dc96f5c80829d8cbf7038fa89dcccdc894
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-01-05 04:20:41 +0000

    Create TLSPlaintext.deserialize

--- lib/raiha/tls/record.rb
+++ lib/raiha/tls/record.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "stringio"
+
 module Raiha
   module TLS
     class Record
@@ -12,6 +14,23 @@ module Raiha
       }.freeze
       LEGACY_PROTOCOL_VERSION = [0x03, 0x03].pack("C*") # TLS v1.2
 
+      # TLSPlaintext class represents the +TLSPlaintext+ struct object.
+      #
+      #     enum {
+      #         invalid(0),
+      #         change_cipher_spec(20),
+      #         alert(21),
+      #         handshake(22),
+      #         application_data(23),
+      #         (255)
+      #     } ContentType;
+      #
+      #     struct {
+      #         ContentType type;
+      #         ProtocolVersion legacy_record_version;
+      #         uint16 length;
+      #         opaque fragment[TLSPlaintext.length];
+      #     } TLSPlaintext;
       # @see https://datatracker.ietf.org/doc/html/rfc8446#section-5.1
       class TLSPlaintext
         FRAGMENT_SIZE_LIMIT = 2**14
@@ -39,7 +58,58 @@ module Raiha
           end
           bufs
         end
+
+        def self.deserialize(bufs)
+          cursor = 0
+          deserialized = []
+          loop do
+            target = bufs[cursor]
+            fragment = unwrap_fragment(target)
+
+            case fragment[:content_type]
+            when CONTENT_TYPE[:handshake]
+              fragment_hs = fragment[:fragment]
+              loop do
+                hs = Handshake.deserialize(fragment_hs)
+                if hs.nil?
+                  cursor += 1
+                  target = bufs[cursor]
+                  fragment = unwrap_fragment(target)
+                  if fragment[:content_type] == CONTENT_TYPE[:handshake]
+                    fragment_hs += fragment[:fragment]
+                  else
+                    raise "unexpected content type: #{fragment[:content_type]}"
+                  end
+                else
+                  deserialized << hs
+                  break
+                end
+              end
+            end
+
+            cursor += 1
+            break if bufs.length <= cursor
+          end
+
+          deserialized
+        end
+
+        def self.unwrap_fragment(serialized_tlsplaintext)
+          buf = StringIO.new(serialized_tlsplaintext)
+          content_type = buf.read(1).unpack1("C")
+          legacy_record_version = buf.read(2)
+          raise "unknown legacy record version: #{legacy_record_version}" unless legacy_record_version == LEGACY_PROTOCOL_VERSION
+
+          length = buf.read(2).unpack1("n")
+          fragment = buf.read(length)
+          if !buf.eof? || fragment.bytesize != length
+            raise "incorrect fragment size: #{fragment.bytesize}, given length: #{length}"
+          end
+
+          { content_type: content_type, fragment: fragment }
+        end
       end
+
 
       # @see https://datatracker.ietf.org/doc/html/rfc8446#section-5.2
       class TLSCiphertext
--- test/raiha/tls/record_test.rb
+++ test/raiha/tls/record_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require "raiha/tls/handshake"
 require "raiha/tls/record"
 
 class RaihaTLSRecordTest < Minitest::Test
@@ -20,5 +21,48 @@ class RaihaTLSRecordTest < Minitest::Test
     record = Raiha::TLS::Record::TLSPlaintext.serialize(handshake)
     assert_equal 2, record.size
     assert_equal 16389, record[0].bytesize # limit of single TLSPlaintext struct
+  end
+
+  def test_tlsplaintext_unwrap_fragment
+    # normal case
+    handshake = Raiha::TLS::Handshake.new.tap do |hs|
+      hs.handshake_type = Raiha::TLS::Handshake::HANDSHAKE_TYPE[:client_hello]
+      hs.message = Raiha::TLS::Handshake::ClientHello.build
+    end
+    record = Raiha::TLS::Record::TLSPlaintext.serialize(handshake)
+    fragment = Raiha::TLS::Record::TLSPlaintext.unwrap_fragment(record[0])
+    assert_equal String, fragment[:fragment].class
+
+    # fragment size is too large
+    assert_raises(RuntimeError) do
+      fragment = Raiha::TLS::Record::TLSPlaintext.unwrap_fragment(record[0] + "\x00")
+    end
+
+    # fragment size is too short
+    assert_raises(RuntimeError) do
+      fragment = Raiha::TLS::Record::TLSPlaintext.unwrap_fragment(record[0][0..-2])
+    end
+  end
+
+  def test_tlsplaintext_deserialize
+    handshake = Raiha::TLS::Handshake.new.tap do |hs|
+      hs.handshake_type = Raiha::TLS::Handshake::HANDSHAKE_TYPE[:client_hello]
+      hs.message = Raiha::TLS::Handshake::ClientHello.build
+    end
+    record = Raiha::TLS::Record::TLSPlaintext.serialize(handshake)
+    deserialized = Raiha::TLS::Record::TLSPlaintext.deserialize(record)
+    assert_equal 1, deserialized.length
+  end
+
+  def test_tlsplaintext_deserialize_fragmented_handshake
+    handshake = Raiha::TLS::Handshake.new.tap do |hs|
+      hs.handshake_type = Raiha::TLS::Handshake::HANDSHAKE_TYPE[:client_hello]
+      hs.message = Raiha::TLS::Handshake::ClientHello.build
+      hs.message.extensions << Raiha::TLS::Handshake::Extension::Padding.generate_padding_with_length(16384)
+    end
+    record = Raiha::TLS::Record::TLSPlaintext.serialize(handshake)
+    deserialized = Raiha::TLS::Record::TLSPlaintext.deserialize(record)
+    assert_equal 1, deserialized.length
+    assert_equal Raiha::TLS::Handshake, deserialized[0].class
   end
 end
