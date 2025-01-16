673453b235f19069a58ced6a4742d7659076c7ed
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-01-16 15:18:24 +0000

    Make server receive ClientHello and build ServerHello

--- lib/raiha/tls/server.rb
+++ lib/raiha/tls/server.rb
@@ -23,12 +23,40 @@ module Raiha
 
       def initialize
         @state = State::START
+        @cipher_suite = nil
+        @client_hello = nil
+        @server_hello = nil
+        @buffer = []
       end
 
-      def receive()
+      def receive(datagrams)
+        case @state
+        when State::START
+          receive_client_hello(datagrams)
+        end
       end
 
-      def receive_client_hello()
+      def receive_client_hello(datagrams)
+        # binding.irb
+        records = Raiha::TLS::Record::TLSPlaintext.deserialize(datagrams)
+        hs = records.first
+        if hs.handshake_type != Raiha::TLS::Handshake::HANDSHAKE_TYPE[:client_hello]
+          raise "TODO: alert? not client hello"
+        end
+
+        @client_hello = hs.message
+
+        unless @cipher_suite = choose_cipher_suite(@client_hello.cipher_suites)
+          raise "TODO: alert? cannot choose cipher suite"
+        end
+
+        transition_state(State::RECVD_CH)
+
+        @server_hello = Raiha::TLS::Handshake::ServerHello.build_from_client_hello(@client_hello)
+      end
+
+      def choose_cipher_suite(cipher_suites)
+        cipher_suites.find(&:supported?)
       end
 
       private def transition_state(state)
