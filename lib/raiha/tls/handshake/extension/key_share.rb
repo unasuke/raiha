c9cfdd939a7677ce2278f1808cf9678a81fe6e1c
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-01-25 18:22:46 +0000

    Impl basic key_share extension serialize/deserialize

--- lib/raiha/tls/handshake/extension/key_share.rb
+++ lib/raiha/tls/handshake/extension/key_share.rb
@@ -1,12 +1,146 @@
 require_relative "abstract_extension"
+require "openssl"
 
 module Raiha
   module TLS
     class Handshake
       class Extension
-        # @see https://datatracker.ietf.org/doc/rfc8446/
+        # Key Share Extension
+        #
+        #   struct {
+        #       NamedGroup group;
+        #       opaque key_exchange<1..2^16-1>;
+        #   } KeyShareEntry;
+        #
+        #   struct {
+        #       KeyShareEntry client_shares<0..2^16-1>;
+        #   } KeyShareClientHello;
+        #
+        #   struct {
+        #       NamedGroup selected_group;
+        #   } KeyShareHelloRetryRequest;
+        #
+        #   struct {
+        #       KeyShareEntry server_share;
+        #   } KeyShareServerHello;
+        #
+        # @see https://datatracker.ietf.org/doc/html/rfc8446#section-4.2.8
         class KeyShare < AbstractExtension
           EXTENSION_TYPE_NUMBER = 51
+
+          NAMED_GROUPS = { # TODO: Move to somewhere (define on specific class)
+            "prime256v1" => "\x00\x17", # secp256r1
+            "secp384r1" => "\x00\x18",
+            "secp521r1" => "\x00\x19",
+            "x25519" => "\x00\x1D",
+            "x448" => "\x00\x1E",
+            "ffdhe2048" => "\x01\x00",
+            "ffdhe3072" => "\x01\x01",
+            "ffdhe4096" => "\x01\x02",
+            "ffdhe6144" => "\x01\x03",
+            "ffdhe8192" => "\x01\x04",
+          }.freeze
+
+          attr_accessor :private_key
+          attr_accessor :ec
+          attr_accessor :groups
+          attr_accessor :key_exchange
+
+          def self.generate_key(groups = ["prime256v1"], on: :client_hello)
+            self.new(on: on).tap do |key_share|
+              key_share.groups = []
+              groups.each do |group|
+                ec = OpenSSL::PKey::EC.generate(group) # TODO: x25519
+                key_share.groups << { group: group, key_exchange: ec.public_key.to_octet_string(:uncompressed) }
+              end
+            end
+          end
+
+          def extension_data=(data)
+            super
+            buf = StringIO.new(data)
+            @groups = []
+            case @on
+            when :client_hello
+              client_shares_length = buf.read(2).unpack1("n")
+              read_client_shares_length = 0
+              loop do
+                group_name = NAMED_GROUPS.key(buf.read(2))
+                read_client_shares_length += 2
+                key_exchange_length = buf.read(2).unpack1("n")
+                read_client_shares_length += 2
+                key_exchange = buf.read(key_exchange_length)
+                read_client_shares_length += key_exchange_length
+                @groups << validate_group_and_key_exchange(group_name, key_exchange)
+
+                if client_shares_length == read_client_shares_length
+                  raise "TODO: mismatch length" unless buf.eof?
+
+                  break
+                end
+              end
+            when :server_hello
+              group_name = NAMED_GROUPS.key(buf.read(2))
+              key_exchange_length = buf.read(2).unpack1("n")
+              key_exchange = buf.read(key_exchange_length)
+              @groups << validate_group_and_key_exchange(group_name, key_exchange)
+            else
+              # TODO
+            end
+          end
+
+          def serialize
+            case @on
+            when :client_hello
+              serialize_for_client_hello
+            when :server_hello
+              serialize_for_server_hello
+            when :hello_retry_request
+              serialize_for_hello_retry_request
+            else
+              # TODO: raise?
+            end
+          end
+
+          private def serialize_for_client_hello
+            key_share_entries = @groups.map do |group|
+              NAMED_GROUPS[group[:group]] + [group[:key_exchange].bytesize].pack("n") + group[:key_exchange]
+            end.join
+
+            key_share_data = [key_share_entries.bytesize].pack("n") + key_share_entries
+
+            [EXTENSION_TYPE_NUMBER].pack("n") + [key_share_data.bytesize].pack("n") + key_share_data
+          end
+
+          private def serialize_for_server_hello
+            raise "on server_hello, only one group is supported" unless @groups.size == 1
+
+            key_share_data = NAMED_GROUPS[@groups.first[:group]] + [@groups.first[:key_exchange].bytesize].pack("n") + @groups.first[:key_exchange]
+
+            [EXTENSION_TYPE_NUMBER].pack("n") + [key_share_data.bytesize].pack("n") + key_share_data
+          end
+
+          private def serialize_for_hello_retry_request
+            raise NotImplementedError
+          end
+
+          private def validate_group_and_key_exchange(group_name, key_exchange)
+            case group_name
+            when "x25519"
+              pkey = OpenSSL::PKey.new_raw_public_key(group_name, key_exchange) # verify given key_exchange is valid
+              { group: group_name, key_exchange: pkey.raw_public_key }
+            when "x448"
+              # TODO: I need test vector...
+              raise "x448"
+            when "prime256v1", "secp384r1", "secp521r1"
+              group = OpenSSL::PKey::EC::Group.new(group_name)
+              point = OpenSSL::PKey::EC::Point.new(group, key_exchange) # verify given key_exchange is valid
+              { group: group_name, key_exchange: point.to_octet_string(:uncompressed) }
+            else
+              # TODO:
+              raise NotImplementedError
+            end
+          end
         end
       end
     end
---
+++ test/raiha/tls/handshake/extension/key_share_test.rb
@@ -0,0 +1,67 @@
+require "test_helper"
+require "openssl"
+require "raiha/tls/handshake/extension"
+
+class RaihaTLSHandshakeExtensionKeyShareTest < Minitest::Test
+  # https://tls13.xargs.org/#client-hello (without extension_type and extension_data length bytes)
+  TLS13_XARGS_ORG_CLIENT_HELLO_KEY_SHARE_DATA = [<<~RANDOM.gsub(/[[:space:]]/, '')].pack("H*")
+    00 24 00 1d 00 20 35 80 72 d6 36 58 80 d1 ae ea 32 9a df
+    91 21 38 38 51 ed 21 a2 8e 3b 75 e9 65 d0 d2 cd 16 62 54
+  RANDOM
+
+  # https://tls13.xargs.org/#server-hello (without extension_type and extension_data length bytes)
+  TLS13_XARGS_ORG_SERVER_HELLO_KEY_SHARE_DATA = [<<~RANDOM.gsub(/[[:space:]]/, '')].pack("H*")
+    00 1d 00 20 9f d7 ad 6d cf f4 29 8d d3 f9 6d 5b 1b 2a
+    f9 10 a0 53 5b 14 88 d7 f8 fa bb 34 9a 98 28 80 b6 15
+  RANDOM
+
+  def test_generate
+    key_share = Raiha::TLS::Handshake::Extension::KeyShare.generate_key
+    assert_equal "prime256v1", key_share.groups.first[:group]
+
+    assert_raises(OpenSSL::PKey::ECError) do
+      Raiha::TLS::Handshake::Extension::KeyShare.generate_key(["unsupported_group"])
+    end
+  end
+
+  def test_serialize_client_hello
+    key_share = Raiha::TLS::Handshake::Extension::KeyShare.generate_key(on: :client_hello)
+    assert_equal String, key_share.serialize.class
+    assert_equal "prime256v1", key_share.groups.first[:group]
+  end
+
+  def test_serialize_server_hello
+    key_share1 = Raiha::TLS::Handshake::Extension::KeyShare.generate_key(["prime256v1"], on: :server_hello)
+    assert_equal String, key_share1.serialize.class
+    assert_equal "prime256v1", key_share1.groups.first[:group]
+
+    key_share2 = Raiha::TLS::Handshake::Extension::KeyShare.generate_key(["prime256v1", "secp384r1"], on: :server_hello)
+    assert_raises do
+      key_share2.serialize
+    end
+  end
+
+  def test_extension_data
+    key_share1 = Raiha::TLS::Handshake::Extension::KeyShare.new(on: :client_hello)
+    key_share1.extension_data = TLS13_XARGS_ORG_CLIENT_HELLO_KEY_SHARE_DATA
+    assert_equal "x25519", key_share1.groups.first[:group]
+
+    key_share2 = Raiha::TLS::Handshake::Extension::KeyShare.new(on: :server_hello)
+    key_share2.extension_data = TLS13_XARGS_ORG_SERVER_HELLO_KEY_SHARE_DATA
+    assert_equal "x25519", key_share2.groups.first[:group]
+  end
+
+  def test_deserialize
+    key_share1 = Raiha::TLS::Handshake::Extension::KeyShare.generate_key(["prime256v1"], on: :client_hello)
+    deserialized1 = Raiha::TLS::Handshake::Extension.deserialize_extensions(key_share1.serialize, type: :client_hello)
+    deserialized_key_share1 = deserialized1.first
+    assert_equal 1, deserialized_key_share1.groups.length
+    assert_equal "prime256v1", deserialized_key_share1.groups.first[:group]
+
+    key_share2 = Raiha::TLS::Handshake::Extension::KeyShare.generate_key(["prime256v1"], on: :server_hello)
+    deserialized2 = Raiha::TLS::Handshake::Extension.deserialize_extensions(key_share2.serialize, type: :server_hello)
+    deserialized_key_share2 = deserialized2.first
+    assert_equal 1, deserialized_key_share2.groups.length
+    assert_equal "prime256v1", deserialized_key_share2.groups.first[:group]
+  end
+end
