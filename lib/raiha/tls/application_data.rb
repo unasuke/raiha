b23e4b4d2567773eca524c931b68b329ef05d669
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-01-27 16:11:14 +0000

    Add ApplicationData

---
+++ lib/raiha/tls/application_data.rb
@@ -0,0 +1,13 @@
+module Raiha
+  module TLS
+    class ApplicationData
+      attr_accessor :content
+
+      def self.deserialize(buf)
+        self.new.tap do |application_data|
+          application_data.content = buf
+        end
+      end
+    end
+  end
+end
