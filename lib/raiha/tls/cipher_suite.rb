dbafc6bd4dc62f7937c6846c14fe39170c71724c
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-02-02 03:10:24 +0900

    Add #name, #aead_algorithm, #hash_algorithm to CipherSuite and...
    add tests.

--- lib/raiha/tls/cipher_suite.rb
+++ lib/raiha/tls/cipher_suite.rb
@@ -9,6 +9,8 @@ module Raiha
 
       SUPPORTED_CIPHER_SUITES = [:TLS_AES_128_GCM_SHA256, :TLS_AES_256_GCM_SHA384, :TLS_CHACHA20_POLY1305_SHA256]
 
+      attr_reader :name
+
       def initialize(cipher_name)
         raise "unknown cipher suite: #{cipher_name.inspect}" unless self.class.constants.include?(cipher_name)
 
@@ -25,6 +27,34 @@ module Raiha
 
       def supported?
         SUPPORTED_CIPHER_SUITES.include?(@name.to_sym)
+      end
+
+      def hash_algorithm
+        @hash_algorithm ||= case @name
+        when :TLS_AES_128_GCM_SHA256, :TLS_CHACHA20_POLY1305_SHA256, :TLS_AES_128_CCM_SHA256
+          "SHA256"
+        when :TLS_AES_256_GCM_SHA384
+          "SHA384"
+        else
+          raise "TODO: hash algorithm for #{@name} is not supported" # TODO: really not supported?
+        end
+      end
+
+      def aead_algorithm
+        @aead_algorithm ||= case @name
+        when :TLS_AES_128_GCM_SHA256
+          "aes-128-gcm"
+        when :TLS_AES_256_GCM_SHA384
+          "aes-256-gcm"
+        when :TLS_CHACHA20_POLY1305_SHA256
+          "chacha20-poly1305"
+        when :TLS_AES_128_CCM_SHA256
+          "aes-128-ccm"
+        when :TLS_AES_128_CCM_8_SHA256
+          raise "TODO: aead algorithm for #{@name} is not supported" # TODO: really not supported?
+        else
+          raise "TODO: aead algorithm for #{@name} is not supported"
+        end
       end
 
       def self.deserialize(data)
