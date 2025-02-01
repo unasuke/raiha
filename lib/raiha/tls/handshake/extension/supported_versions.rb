d9bc790bd74d6463c2f7dba16ca6f565f2096b0e
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-02-02 01:28:25 +0900

    Refine SupportedVersions extension
    support client_hello and server_hello

--- lib/raiha/tls/handshake/extension/supported_versions.rb
+++ lib/raiha/tls/handshake/extension/supported_versions.rb
@@ -19,9 +19,40 @@ module Raiha
         class SupportedVersions < AbstractExtension
           EXTENSION_TYPE_NUMBER = 43
 
+          attr_accessor :protocol_versions
+
           def self.generate_for_tls13
-            self.new(on: :client_hello).tap do |ext| # message type independent
+            self.new(on: :client_hello).tap do |ext|
               ext.extension_data = "\x02\x03\x04"
+            end
+          end
+
+          def extension_data=(data)
+            super
+
+            @protocol_versions = []
+            buf = StringIO.new(data)
+            case @on
+            when :client_hello
+              length = buf.read(1).unpack1("C") / 2
+              length.times.map { @protocol_versions << buf.read(2) }
+            when :server_hello
+              @protocol_versions << buf.read(2)
+            end
+          end
+
+          def serialize
+            case @on
+            when :client_hello
+              data = [@protocol_versions.length * 2].pack("C") + @protocol_versions.join
+              [EXTENSION_TYPE_NUMBER].pack("n") + [data.bytesize].pack("n") + data
+            when :server_hello
+              raise if @protocol_versions.length > 1
+
+              data = @protocol_versions.join
+              [EXTENSION_TYPE_NUMBER].pack("n") + [data.bytesize].pack("n") + data
+            else
+              # TODO: raise?
             end
           end
         end
--- test/raiha/tls/handshake/extension/supported_versions_test.rb
+++ test/raiha/tls/handshake/extension/supported_versions_test.rb
@@ -5,11 +5,19 @@ class RaihaTLSHandshakeExtensionSupportedVersionsTest < Minitest::Test
   def test_generate_for_tls13
     ext = Raiha::TLS::Handshake::Extension::SupportedVersions.generate_for_tls13
     assert_equal Raiha::TLS::Handshake::Extension::SupportedVersions, ext.class
-    assert_equal "\x02\x03\x04", ext.extension_data
+    assert_equal_bin "\x02\x03\x04", ext.extension_data
+    assert_equal ["\x03\x04"], ext.protocol_versions
   end
 
-  def test_serialize
-    ext = Raiha::TLS::Handshake::Extension::SupportedVersions.generate_for_tls13
-    assert_equal "\x00\x2b\x00\x03\x02\x03\x04", ext.serialize
+  def test_serialize_for_client_hello
+    ext = Raiha::TLS::Handshake::Extension::SupportedVersions.new(on: :client_hello)
+    ext.protocol_versions = ["\x03\x04"]
+    assert_equal_bin "\x00\x2b\x00\x03\x02\x03\x04", ext.serialize
+  end
+
+  def test_serialize_for_server_hello
+    ext = Raiha::TLS::Handshake::Extension::SupportedVersions.new(on: :server_hello)
+    ext.protocol_versions = ["\x03\x04"]
+    assert_equal_bin "\x00\x2b\x03\x04", ext.serialize
   end
 end
