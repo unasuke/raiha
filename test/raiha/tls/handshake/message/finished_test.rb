325c27edfa06b3c79e4ae5c4eb1245ffb23b8be4
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-02-09 22:09:10 +0900

    Add Finished#serialize

--- test/raiha/tls/handshake/message/finished_test.rb
+++ test/raiha/tls/handshake/message/finished_test.rb
@@ -14,4 +14,10 @@ class RaihaTLSHandshakeFinishedTest < Minitest::Test
     assert_equal Raiha::TLS::Handshake::Finished, handshake.message.class
     assert_equal OpenSSL::Digest.new("sha256").digest_length, handshake.message.verify_data.bytesize
   end
+
+  def test_serialize_rfc8448
+    handshake = Raiha::TLS::Handshake.deserialize(RFC8448_1RTT_SERVER_HANDSHAKE_FINISHED)
+    serialized = handshake.serialize
+    assert_equal_bin RFC8448_1RTT_SERVER_HANDSHAKE_FINISHED, serialized
+  end
 end
