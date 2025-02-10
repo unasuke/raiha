b8678daed2112068e01129ad68965fdf5540a47f
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-02-10 15:48:18 +0900

    Grouping test vectors from rfc8448 to single file
    * tweak consts naming

--- test/raiha/tls/handshake_test.rb
+++ test/raiha/tls/handshake_test.rb
@@ -1,35 +1,7 @@
 require "test_helper"
+require "support/rfc8448_test_vector"
 require "raiha/tls/handshake"
 require "raiha/tls/cipher_suite"
-
-# https://datatracker.ietf.org/doc/html/rfc8448#section-3
-RFC8448_SIMPLE_1RTT_HANDSHAKE_CLIENT_HELLO = [<<~HS.gsub(/[[:space:]]/, '')].pack("H*")
-  01 00 00 c0 03 03 cb 34 ec b1 e7 81 63
-  ba 1c 38 c6 da cb 19 6a 6d ff a2 1a 8d 99 12 ec 18 a2 ef 62 83
-  02 4d ec e7 00 00 06 13 01 13 03 13 02 01 00 00 91 00 00 00 0b
-  00 09 00 00 06 73 65 72 76 65 72 ff 01 00 01 00 00 0a 00 14 00
-  12 00 1d 00 17 00 18 00 19 01 00 01 01 01 02 01 03 01 04 00 23
-  00 00 00 33 00 26 00 24 00 1d 00 20 99 38 1d e5 60 e4 bd 43 d2
-  3d 8e 43 5a 7d ba fe b3 c0 6e 51 c1 3c ae 4d 54 13 69 1e 52 9a
-  af 2c 00 2b 00 03 02 03 04 00 0d 00 20 00 1e 04 03 05 03 06 03
-  02 03 08 04 08 05 08 06 04 01 05 01 06 01 02 01 04 02 05 02 06
-  02 02 02 00 2d 00 02 01 01 00 1c 00 02 40 01
-HS
-
-# https://datatracker.ietf.org/doc/html/rfc8448#section-3
-RFC8448_SIMPLE_1RTT_HANDSHAKE_CLIENT_HELLO_RANDOM = [<<~RANDOM.gsub(/[[:space:]]/, '')].pack("H*")
-  cb 34 ec b1 e7 81 63 ba 1c 38 c6 da cb 19 6a 6d
-  ff a2 1a 8d 99 12 ec 18 a2 ef 62 83 02 4d ec e7
-RANDOM
-
-# https://datatracker.ietf.org/doc/html/rfc8448#section-3
-RFC8448_SIMPLE_1RTT_HANDSHAKE_SERVER_HELLO = [<<~SH.gsub(/[[:space:]]/, '')].pack("H*")
-  02 00 00 56 03 03 a6 af 06 a4 12 18 60
-  dc 5e 6e 60 24 9c d3 4c 95 93 0c 8a c5 cb 14 34 da c1 55 77 2e
-  d3 e2 69 28 00 13 01 00 00 2e 00 33 00 24 00 1d 00 20 c9 82 88
-  76 11 20 95 fe 66 76 2b db f7 c6 72 e1 56 d6 cc 25 3b 83 3d f1
-  dd 69 b1 b0 4e 75 1f 0f 00 2b 00 02 03 04
-SH
 
 class RaihaTLSHandShakeTest < Minitest::Test
   def test_serialize_client_hello
