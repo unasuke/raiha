46899c6ab5a2721fb484e517b0743f43b2448133
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-03-03 00:26:23 +0900

    Add serialize and deserialize test for some handshake messages
    * Certificate
    * CertificateVerify
    * EncryptedExtensions

--- test/raiha/tls/handshake/message/certificate_test.rb
+++ test/raiha/tls/handshake/message/certificate_test.rb
@@ -16,4 +16,46 @@ class RaihaTLSHandshakeCertificateTest < Minitest::Test
     handshake = Raiha::TLS::Handshake.deserialize(RFC8448_SIMPLE_1RTT_HANDSHAKE_SERVER_CERTIFICATE)
     assert_equal_bin RFC8448_SIMPLE_1RTT_HANDSHAKE_SERVER_CERTIFICATE, handshake.serialize
   end
+
+  def test_serialize_and_deserialize
+    cert = generate_temporary_rsa_certificate
+    handshake = Raiha::TLS::Handshake.new.tap do |hs|
+      hs.handshake_type = Raiha::TLS::Handshake::HANDSHAKE_TYPE[:certificate]
+      hs.message = Raiha::TLS::Handshake::Certificate.new.tap do |cv|
+        cv.opaque_certificate_data = cert.to_der
+      end
+    end
+
+    serialized = handshake.serialize
+    deserialized = Raiha::TLS::Handshake.deserialize(serialized)
+    assert_equal Raiha::TLS::Handshake::Certificate, deserialized.message.class
+    assert_equal cert.to_der, deserialized.message.opaque_certificate_data
+    assert_equal_bin deserialized.serialize, serialized
+  end
+
+  def generate_temporary_rsa_certificate
+    rsa = OpenSSL::PKey::RSA.generate(2048)
+    csr = OpenSSL::X509::Request.new
+    name = OpenSSL::X509::Name.new
+    name.add_entry("CN", "localhost")
+    name.add_entry("DC", "localhost")
+    csr.subject = name
+    csr.version = 0
+    csr.public_key = rsa.public_key
+    factory = OpenSSL::X509::ExtensionFactory.new
+    exts = [factory.create_ext("subjectAltName", "DNS:localhost")]
+    asn1exts = OpenSSL::ASN1::Set([OpenSSL::ASN1::Sequence(exts)])
+    csr.add_attribute(OpenSSL::X509::Attribute.new("extReq", asn1exts))
+    csr.sign(rsa, "sha256")
+    cert = OpenSSL::X509::Certificate.new
+    cert.serial = 0
+    cert.version = 2
+    cert.not_before = Time.now
+    cert.not_after = Time.now + 60 # 1 minutes
+    cert.subject = name
+    cert.issuer = name
+    cert.public_key = csr.public_key
+    cert.sign(rsa, "sha256")
+    cert
+  end
 end
--- test/raiha/tls/handshake/message/certificate_verify_test.rb
+++ test/raiha/tls/handshake/message/certificate_verify_test.rb
@@ -14,4 +14,20 @@ class RaihaTLSHandshakeCertificateVerifyTest < Minitest::Test
     serialized = handshake.serialize
     assert_equal_bin RFC8448_SIMPLE_1RTT_HANDSHAKE_SERVER_CERTIFICATE_VERIFY, serialized
   end
+
+  def test_serialize_and_deserialize
+    handshake = Raiha::TLS::Handshake.new.tap do |hs|
+      hs.handshake_type = Raiha::TLS::Handshake::HANDSHAKE_TYPE[:certificate_verify]
+      hs.message = Raiha::TLS::Handshake::CertificateVerify.new.tap do |cv|
+        cv.algorithm = "rsa_pss_rsae_sha256"
+        cv.signature = "sample-signature"
+      end
+    end
+    serialized = handshake.serialize
+    deserialized = Raiha::TLS::Handshake.deserialize(serialized)
+    assert_equal Raiha::TLS::Handshake::CertificateVerify, deserialized.message.class
+    assert_equal "rsa_pss_rsae_sha256", deserialized.message.algorithm
+    assert_equal "sample-signature", deserialized.message.signature
+    assert_equal_bin deserialized.serialize, serialized
+  end
 end
