9f8e81d8483d7259b77bfa6f0264fa201f369a91
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-05-03 16:54:04 +0900

    Add definition if tls alert message as comment

--- lib/raiha/tls/alert.rb
+++ lib/raiha/tls/alert.rb
@@ -5,6 +5,45 @@ require_relative "../../tls"
 module Raiha
   module TLS
     # Represents alerts of the TLS 1.3 protocol.
+    #
+    #  enum { warning(1), fatal(2), (255) } AlertLevel;
+    #
+    #  enum {
+    #      close_notify(0),
+    #      unexpected_message(10),
+    #      bad_record_mac(20),
+    #      record_overflow(22),
+    #      handshake_failure(40),
+    #      bad_certificate(42),
+    #      unsupported_certificate(43),
+    #      certificate_revoked(44),
+    #      certificate_expired(45),
+    #      certificate_unknown(46),
+    #      illegal_parameter(47),
+    #      unknown_ca(48),
+    #      access_denied(49),
+    #      decode_error(50),
+    #      decrypt_error(51),
+    #      protocol_version(70),
+    #      insufficient_security(71),
+    #      internal_error(80),
+    #      inappropriate_fallback(86),
+    #      user_canceled(90),
+    #      missing_extension(109),
+    #      unsupported_extension(110),
+    #      unrecognized_name(112),
+    #      bad_certificate_status_response(113),
+    #      unknown_psk_identity(115),
+    #      certificate_required(116),
+    #      no_application_protocol(120),
+    #      (255)
+    #  } AlertDescription;
+    #
+    #  struct {
+    #      AlertLevel level;
+    #      AlertDescription description;
+    #  } Alert;
+    #
     # @see https://datatracker.ietf.org/doc/html/rfc8446#section-6
     class Alert
       # Abstract base class for all alerts.
