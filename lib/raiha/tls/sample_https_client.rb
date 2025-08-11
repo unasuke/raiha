c6a72573d1f0717922e6835935830f4e992d8cb6
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-08-11 20:58:57 +0900

    Implement SampleHttpsClient class for handling HTTPS connections

---
+++ lib/raiha/tls/sample_https_client.rb
@@ -0,0 +1,65 @@
+require "socket"
+require_relative "client"
+
+module Raiha
+  module TLS
+    class SampleHttpsClient
+      def self.start
+        client = self.new
+        begin
+          client.connect!
+          client.send(<<~HTTPGET)
+            GET / HTTP/1.1\r
+            Host: www.example.com\r
+            Connection: close\r
+
+          HTTPGET
+          pp client.read
+        ensure
+          client.close
+        end
+      end
+
+      def initialize
+        @client = Raiha::TLS::Client.new
+        @host = "www.example.com"
+        @port = 443
+        @socket = TCPSocket.new(@host, @port)
+      end
+
+      def connect!
+        loop do
+          break if @client.finished?
+
+          begin
+            @client.datagrams_to_send&.each do |datagram|
+              @socket.sendmsg(datagram)
+            end
+            response = @socket.recvmsg_nonblock
+            @client.receive2(response.first)
+          rescue IO::WaitReadable
+            next
+          end
+        end
+      end
+
+      def read
+        loop do
+          response = @socket.recvmsg_nonblock
+          # pp "response:" + response.first
+          @client.receive2(response.first)
+        rescue IO::WaitReadable
+          next
+        end
+      end
+
+      def close
+        @socket&.close
+      end
+
+      def send(data)
+        @socket.sendmsg(@client.encrypt_application_data(data))
+      end
+    end
+  end
+end
