237eef5d134443db31c06f3e3d6ac80a61e45b57
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-08-28 01:44:19 +0900

    Impl and test ServerName extension

--- lib/raiha/tls/handshake/extension/server_name.rb
+++ lib/raiha/tls/handshake/extension/server_name.rb
@@ -4,7 +4,25 @@ module Raiha
   module TLS
     class Handshake
       class Extension
-        # @see https://datatracker.ietf.org/doc/rfc6066/
+        # ServerName Extension
+        #   struct {
+        #       NameType name_type;
+        #       select (name_type) {
+        #           case host_name: HostName;
+        #       } name;
+        #   } ServerName;
+        #
+        #   enum {
+        #       host_name(0), (255)
+        #   } NameType;
+        #
+        #   opaque HostName<1..2^16-1>;
+        #
+        #   struct {
+        #       ServerName server_name_list<1..2^16-1>
+        #   } ServerNameList;
+        #
+        # @see https://datatracker.ietf.org/doc/html/rfc6066#section-3
         class ServerName < AbstractExtension
           EXTENSION_TYPE_NUMBER = 0
           attr_accessor :server_name
@@ -12,6 +30,13 @@ module Raiha
           def extension_data=(data)
             super
             @server_name = @extension_data
+          end
+
+          def serialize
+            server_name = [0].pack("C") + # host_name(0)
+              [@server_name.bytesize].pack("n") + @server_name # HostName
+            server_name_list = [server_name.bytesize].pack("n") + server_name
+            [EXTENSION_TYPE_NUMBER].pack("n") + [server_name_list.bytesize].pack("n") + server_name_list
           end
         end
       end
---
+++ test/raiha/tls/handshake/extension/server_name_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+require "raiha/tls/handshake/extension"
+
+class RaihaTLSHandshakeExtensionServerNameTest < Minitest::Test
+  TLS13_SERVER_NAME_EXTENSION_DATA_WWW_EXAMPLE_COM = [<<~DATA.gsub(/[[:space:]]/, '')].pack("H*")
+    00 00 00 14 00 12 00 00 0f 77 77 77 2e 65 78 61
+    6d 70 6c 65 2e 63 6f 6d
+  DATA
+
+  TLS13_SERVER_NAME_EXTENSION_DATA_GOOGLE_COM = [<<~DATA.gsub(/[[:space:]]/, '')].pack("H*")
+    00 00 00 0f 00 0d 00 00 0a 67 6f 6f 67 6c 65 2e
+    63 6f 6d
+  DATA
+  def test_serialize
+    www_example_com = Raiha::TLS::Handshake::Extension::ServerName.new(on: :client_hello)
+    www_example_com.server_name = "www.example.com"
+    assert_equal TLS13_SERVER_NAME_EXTENSION_DATA_WWW_EXAMPLE_COM, www_example_com.serialize
+
+    google_com = Raiha::TLS::Handshake::Extension::ServerName.new(on: :client_hello)
+    google_com.server_name = "google.com"
+    assert_equal TLS13_SERVER_NAME_EXTENSION_DATA_GOOGLE_COM, google_com.serialize
+  end
+end
